9b049ede6da7dd2d6ce3e0bc4c377ed8c1027746
joshh <joshuahinojognzlz@gmail.com>
2025-10-19 03:52:20 -0400

    comments moved

--- src/movegen.h
+++ src/movegen.h
@@ -73,4 +73,26 @@ bool is_attacked(const Position& position, Square square, int attacking_color);
 Bitboard attacked_by(const Position& position, Square square, int attacking_color);
 bool is_king_in_check(const Position& position, int attacking_color);
 
+// Magic Bitnboard implementation (WIP)
+
+//void init_magics(bool is_rook, Bitboard piece_table[], Magic magics[]);
+//struct Magic
+//{
+//	Bitboard* attacks;
+//	Bitboard mask;
+//	Bitboard magic;
+//	int shift;
+
+//	unsigned index(Bitboard occ)
+//	{
+//		return unsigned(((occ & mask) * magic) >> shift);
+//	}
+//};
+
+//Bitboard rook_table[0x19000];
+//Bitboard bishop_table[0x1480];
+
+//Magic rook_magics[64];
+//Magic bishop_magics[64];
+
 #endif // MOVEGEN_H
